3663bff58d3823ee82e792982bab610961bc74ab
Administrator <Administrator@PC201908161927>
2020-01-17 15:43:58 +0800

    dicyi

---
+++ test/src/test/Test.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Test {
+	public static void test(String s) {
+		char[] c = s.toCharArray();
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if(c[i]==c[j]) {
+					System.out.println(c[i]);
+					return;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test("asdfghjklopiuu");
+	}
+
+}
